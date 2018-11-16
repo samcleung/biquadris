96c3dc9e7df8847ec2f0264a747697f15ffbde9a
d68xu <d68xu@student.cs.uwaterloo.ca>
2018-11-16 04:24:25 -0500

    Created player.cc

---
+++ player/player.cc
@@ -0,0 +1,30 @@
+#include <string>
+#include "../player/player.h"
+//#include "../game/game.h"
+//#include "../grid/grid.h"
+//#include "../level/level.h"
+//#include "../block/block.h"
+
+// Basic Constructor
+//Player::Player() :
+    ///
+
+
+//class Player {
+//	std::string name;
+//	std::string scriptFile;
+//	Game::Effect effect = Game::Effect::None;
+//	unsigned int score = 0;
+//	Game* game;
+//	Grid* grid;
+//	Level* level;
+//	Block* current;
+//	
+//	public:
+//	// add ctrs & Big 5 as needed
+//	void turn();
+//	void print(int);
+//	void setEffect(Game::Effect);
+//};
+//
+
